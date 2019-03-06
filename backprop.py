b75dc54fbb4fe1ec0af41e5151532adf4be8364c
Brian Farris <bfarris@steinadmins-mbp.lan>
2019-03-05 23:59:44 -0500

    works

--- backprop.py
+++ backprop.py
@@ -2,13 +2,10 @@ import numpy as np
 
 
 class BackProp:
-    def __init__(self, x, y, loss, step_size):
+    def __init__(self, x, y):
         self.stack = []
         self.x = x
         self.y = y
-        self.loss = loss
-        self.loss.grad_value = 1.0
-        self.step_size = step_size
 
     def forward(self, vertex):
         inputs = [self.forward(edge).value for edge in vertex.edges]
@@ -16,7 +13,7 @@ class BackProp:
         self.stack.append(vertex)
         return vertex
 
-    def backward(self):
+    def backward(self, step_size):
         while self.stack:
             vertex = self.stack.pop()
             inputs = [edge.value for edge in vertex.edges]
@@ -24,17 +21,5 @@ class BackProp:
             for i, edge in enumerate(vertex.edges):
                 edge.grad_value += grads[i]
             if vertex.trainable:
-                vertex.value -= (self.step_size * vertex.grad_value)
+                vertex.value -= (step_size * vertex.grad_value)
             vertex.grad_value = 0.0
-
-    def batch(self, x_batch, y_batch):
-        self.x.value = x_batch
-        self.y.value = y_batch
-        self.forward(self.loss)
-        self.backward()
-
-    def predict(self, x, y):
-        self.x.value = x
-        self.y.value = y
-        self.forward(self.loss)
-        return self.loss.edges[0].value
--- nn.py
+++ nn.py
@@ -10,49 +10,72 @@ if __name__ == "__main__":
     y = Input(name="y")
 
     w1 = Input(name="w1", trainable=True)
-    w1.value = np.random.normal(0, 0.001, (2, 4))
+    w1.value = np.random.normal(0, 0.0001, (2, 4))
     b1 = Input(name="b1", trainable=True)
-    b1.value = np.random.normal(0, 0.001, (1, 4))
+    b1.value = np.zeros((1, 4))
 
     dot1 = Dot([x, w1], name="dot1")
     h1 = AddBias([dot1, b1], name="add1")
     h1_relu = Relu([h1], name="relu1")
 
-    w1 = Input(name="w2", trainable=True)
-    w1.value = np.random.normal(0, 0.001, (4, 2))
-    b1 = Input(name="b2", trainable=True)
-    b1.value = np.random.normal(0, 0.001, (1, 2))
+    w2 = Input(name="w2", trainable=True)
+    w2.value = np.diag(np.random.normal(1, 0.0001, 4))
+    b2 = Input(name="b2", trainable=True)
+    b2.value = np.zeros((1, 4))
 
-    dot1 = Dot([h1_relu, w1], name="dot2")
-    h1 = AddBias([dot1, b1], name="add2")
+    dot2 = Dot([h1_relu, w2], name="dot2")
+    h2 = AddBias([dot2, b2], name="add2")
+    h2_relu = Relu([h2], name="relu2")
 
-    p = Softmax([h1], name="softmax")
+    w3 = Input(name="w3", trainable=True)
+    w3.value = np.random.normal(0, 0.0001, (4, 2))
+    b3 = Input(name="b3", trainable=True)
+    b3.value = np.zeros((1, 2))
+
+    dot3 = Dot([h2_relu, w3], name="dot3")
+    h3 = AddBias([dot3, b3], name="add3")
+
+    p = Softmax([h3], name="softmax")
     L = CrossEntropy([p, y], name="cross_entropy")
+    L.grad_value = 1.0
 
-    backprop = BackProp(x, y, L, 0.001)
+    backprop = BackProp(x, y)
 
-    N = int(1e4)
-    np.random.seed(42)
-    x_full = np.random.normal(0, 1, (N, 2))
-    noise = np.random.normal(0, .01, N)
-    y_full = (np.sqrt(x_full[:, 0]**2 + x_full[:, 1]**2) < (1+noise)).astype(int)
-    y_full = np.vstack([y_full, (1-y_full)]).T
+    N = int(1e5)
+    #np.random.seed(42)
+
+    x_in = np.random.normal(0, 1, (N//2, 2))
+    r = np.random.normal(6,0.5, (N//2, 1))
+    phi = np.random.uniform(0, 2. * np.pi, (N//2, 1))
+    x_out = np.hstack([r * np.cos(phi), r * np.sin(phi)])
+    x_full = np.vstack([x_in, x_out])
+    y_in = np.vstack([np.zeros((N//2, 1)), np.ones((N//2, 1))])
+    y_out = np.vstack([np.ones((N//2, 1)), np.zeros((N//2, 1))])
+    y_full = np.hstack([y_in, y_out])
     x_train, x_test, y_train, y_test = train_test_split(x_full, y_full, test_size=0.1)
 
     loss_history = []
     val_loss_history = []
-    for epoch in range(2000):
-        if epoch % 100 == 0:
+    lr = 1.e-3
+    for epoch in range(20):
+        if epoch % 1 == 0:
             print(epoch)
         for i in range(x_train.shape[0] // 10):
             x_batch = x_train[i * 10: (i+1)*10, :]
             y_batch = y_train[i * 10: (i+1)*10, :]
-            backprop.batch(x_batch, y_batch)
-        loss_history.append(backprop.loss.value)
+
+            backprop.x.value = x_batch
+            backprop.y.value = y_batch
+
+            backprop.forward(L)
+            backprop.backward(lr)
+
+        loss_history.append(L.value)
+
         backprop.x.value = x_test
         backprop.y.value = y_test
-        backprop.forward(backprop.loss)
-        val_loss_history.append(backprop.loss.value)
+        backprop.forward(L)
+        val_loss_history.append(L.value)
 
     plt.figure(figsize=(4, 8))
     plt.subplot(2, 1, 1)
@@ -60,10 +83,13 @@ if __name__ == "__main__":
     plt.plot(loss_history)
     plt.plot(val_loss_history)
 
-    y_pred = backprop.predict(x_test, y_test)
+    backprop.x.value = x_test
+    backprop.y.value = y_test
+    backprop.forward(L)
+    y_pred = L.edges[0].value
 
     plt.subplot(2, 1, 2)
     plt.scatter(x_test[:, 0], x_test[:, 1], c=y_pred[:, 0])
-    plt.xlim(-3, 3)
-    plt.ylim(-3, 3)
+    #plt.xlim(-3, 3)
+    #plt.ylim(-3, 3)
     plt.show()
